01e90a9bd54948898560b0b1461dd4273e427082
sruiz06 <69258600+sruiz06@users.noreply.github.com>
2020-08-05 10:07:16 -0700

    Update HelloWorld.cpp

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -1,12 +1,12 @@
 // HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    cout << "Hello World!\n"; 
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
